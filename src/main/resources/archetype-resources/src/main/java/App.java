39cbf684f98d9c17a2168141693aa36a21be1e33
shamikm <majumdar.shamik@gmail.com>
2014-02-21 18:14:13 -0500

    Initial Commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $us.sam.oip;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
